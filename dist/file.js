9ceb5199b431ae1edc268d500ba6b7103aa6646a
mahendra1729 <mahendra1729@yahoo.com>
2024-05-16 22:48:05 +0530

    additional typescript details added.

--- dist/file.js
+++ dist/file.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 let x = 20;
 if (x > 10)
     x += 10;
@@ -23,4 +24,47 @@ function theFunction(income, taxYears) {
 }
 console.log(theFunction(1001, 2000));
 console.log(theFunction(1001));
+let employee = {
+    id: 1, name: "Mahendra",
+    retire: (date) => console.log(date)
+};
+let emp = {
+    id: 1, name: "Mahendra",
+    retire: (date) => console.log(date)
+};
+function UnionFunction(weight) {
+    if (typeof weight === "number") {
+        return weight;
+    }
+    else {
+        return parseInt(weight);
+    }
+}
+console.log(UnionFunction(12));
+console.log(UnionFunction("14"));
+let textBox = {
+    drag: () => { },
+    resize: () => { }
+};
+let quentity;
+quentity = 100;
+console.log(quentity);
+quentity = 50;
+console.log(quentity);
+quentity = "Mahi";
+console.log(quentity);
+function getCustomerDate(val) {
+    return val === 0 ? null : { birthday: new Date() };
+}
+let cust = getCustomerDate(1);
+if (cust === null) {
+    console.log("customer not exist");
+}
+else if (cust === undefined) {
+    console.log("not defined");
+}
+else {
+    console.log(cust.birthday);
+}
+console.log((_a = cust === null || cust === void 0 ? void 0 : cust.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
 //# sourceMappingURL=file.js.map
--- src/file.ts
+++ src/file.ts
@@ -41,3 +41,110 @@ function theFunction(income : number, taxYears? : number) : number
 
 console.log(theFunction(1001,2000));
 console.log(theFunction(1001));
+
+//object defining
+
+let employee : {
+    readonly id : number; //preventing fot further modifiocation
+    name? : string; //making name optional but not suggested
+    retire : (date : Date) => void;
+} =  {
+    id : 1,  name : "Mahendra",
+    retire : (date : Date) =>
+        console.log(date)
+}
+
+//object defining in more neat way
+type Employee =
+{
+    readonly id : number; //preventing fot further modifiocation
+    name? : string; //making name optional but not suggested
+    retire : (date : Date) => void;
+};
+
+let emp : Employee = 
+{
+    id : 1,  name : "Mahendra",
+    retire : (date : Date) =>
+        console.log(date)
+}
+
+
+//union type in functoin 
+function UnionFunction(weight: number | string) : number 
+{
+    if(typeof weight === "number")
+        {
+            return weight;
+
+        }
+    else
+    {
+        return parseInt(weight);
+    }
+}
+
+console.log(UnionFunction(12));
+console.log(UnionFunction("14"));
+//to use below code we need to disable the strictNullChecks if we are not taking null as input in function
+//console.log(UnionFunction(null));
+
+
+//intersaction types
+type Draggable =
+{
+    drag: () => void
+};
+
+type resizable =
+{
+    resize: () => void
+};
+
+type UIWidger = Draggable & resizable;
+
+let textBox : UIWidger =
+{
+    drag: () => {},
+    resize: () => {}
+}
+
+//Literal values with unions
+let quentity : 50|100|"Mahi";
+
+quentity = 100;
+console.log(quentity);
+quentity = 50;
+console.log(quentity);
+quentity = "Mahi";
+console.log(quentity);
+// quentity = 51;  not possible
+
+//optional chaining
+
+type Customer = 
+{
+    birthday : Date
+};
+
+function getCustomerDate(val : number ) : Customer | null | undefined
+{
+
+    return val === 0 ? null : {birthday : new Date()};
+}
+
+let cust  = getCustomerDate(1);
+
+if(cust === null)
+    {
+        console.log("customer not exist");
+    }
+else if(cust === undefined){
+    console.log("not defined");
+}
+else{
+    console.log(cust.birthday);
+}
+
+//instead of using if else we can use optinal chaining
+console.log(cust?.birthday?.getFullYear());
